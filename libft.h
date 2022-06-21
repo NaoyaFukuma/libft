ad45f2ec9f241a8cac996095f2273e3c4ce57e6b
nfukuma <fukumanaoya@gmail.com>
2022-06-21 10:55:17 +0900

    add function ft_isprint to libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 21:09:46 by nfukuma           #+#    #+#             */
-/*   Updated: 2022/06/21 10:51:00 by nfukuma          ###   ########.fr       */
+/*   Updated: 2022/06/21 10:54:50 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isascii(int c);
+int	ft_isprint(int c);
 
 #endif
