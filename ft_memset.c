f1b89bcc8301000e0030f059434dc6783b589e59
nfukuma <fukumanaoya@gmail.com>
2022-06-28 11:20:34 +0900

    new ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,25 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/18 21:09:46 by nfukuma           #+#    #+#             */
-/*   Updated: 2022/06/28 11:18:38 by nfukuma          ###   ########.fr       */
+/*   Created: 2022/05/20 23:08:17 by nfukuma           #+#    #+#             */
+/*   Updated: 2022/06/17 11:38:58 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef LIBFT_H
-	# define LIBFT_H
-	# include <stdlib.h>
-	# include <unistd.h>
+#include "libft.h"
 
-	int	ft_isalpha(int c);
-	int	ft_isdigit(int c);
-	int	ft_isascii(int c);
-	int	ft_isprint(int c);
-	size_t	ft_strlen(const char *str);
-	void	*ft_memset(void *buf, int c, size_t size);
+void	*ft_memset(void *buf, int c, size_t size)
+{
+	unsigned char	*buf_cpy;
 
-#endif
+	buf_cpy = (unsigned char *)buf;
+	while (size--)
+		*buf_cpy++ = (unsigned char)c;
+	return (buf);
+}
