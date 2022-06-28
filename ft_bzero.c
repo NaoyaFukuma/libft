81adf956831b14511976cebaed382b475c9004bc
nfukuma <fukumanaoya@gmail.com>
2022-06-28 11:25:31 +0900

    ft_bzero.c new

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,26 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/18 21:09:46 by nfukuma           #+#    #+#             */
-/*   Updated: 2022/06/28 11:24:52 by nfukuma          ###   ########.fr       */
+/*   Created: 2022/06/02 11:36:20 by nfukuma           #+#    #+#             */
+/*   Updated: 2022/06/17 11:22:17 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef LIBFT_H
-	# define LIBFT_H
-	# include <stdlib.h>
-	# include <unistd.h>
+#include "libft.h"
 
-	int	ft_isalpha(int c);
-	int	ft_isdigit(int c);
-	int	ft_isascii(int c);
-	int	ft_isprint(int c);
-	size_t	ft_strlen(const char *str);
-	void	*ft_memset(void *buf, int c, size_t size);
-	void	ft_bzero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n)
+{
+	char	*tmp;
 
-#endif
+	tmp = (char *)s;
+	while (n--)
+		*tmp++ = 0;
+}
