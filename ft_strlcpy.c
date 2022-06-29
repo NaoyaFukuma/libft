5e3eae7e31bab6810148c7acafbb2174bc22a398
nfukuma <fukumanaoya@gmail.com>
2022-06-29 10:21:20 +0900

    ft_strlcpy.c new

---
+++ ft_strlcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/20 00:33:40 by nfukuma           #+#    #+#             */
+/*   Updated: 2022/06/17 10:53:27 by nfukuma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	len;
+
+	len = ft_strlen(src);
+	if (!size)
+		return (len);
+	while (*src && --size)
+		*dst++ = *src++;
+	*dst = '\0';
+	return (len);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 21:09:46 by nfukuma           #+#    #+#             */
-/*   Updated: 2022/06/29 10:16:11 by nfukuma          ###   ########.fr       */
+/*   Updated: 2022/06/29 10:20:58 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@
 	void	*ft_memset(void *buf, int c, size_t size);
 	void	ft_bzero(void *s, size_t n);
 	void	*ft_memmove(void *dst, const void *src, size_t size);
+	size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
 
 
 #endif
